363d936590e95f66f5da19ae6ce5c2605691df18
Cumbie <bcumbie@CSM2-0244-MFXXH.main.local.una.edu>
2026-06-25 09:46:07 -0500

    concept

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,53 @@
+  let choice = false;
+  let answer = false; 
+  let score = 0; 
+
 // $(document).ready(
 $(() => { //super shorthand 
   //IIFE => immediately invoked function expression. 
   //arrow function 
 
+
   console.log('jq doc is ready');
+
+  // listen for button clicks. 
+  $('#choiceBtn').on('click', function () {
+    console.log('clicked choice btn')
+    $(this).toggleClass('btn-success');
+    choice = true;
+    if(choice == answer){
+    score += 1;
+    $('#gameScore').text(`Score: ${score}`);
+    // return 1; //if true, win with score +1  
+  }
+  });
+
+  $('#answerBtn').on('click', function () {
+    console.log('clicked answer btn')
+    $(this).toggleClass('btn-success');
+    answer = true; 
+    if(choice == answer){
+    score += 1;
+    $('#gameScore').text(`Score: ${score}`);
+    // return 1; //if true, win with score +1  
+  }
+  });
+
+  // function updateScore() {
+
+  //   $('#gameScore').text(`Score:adadsfasd `);
+
+  // };
+
 
   $('#playerName').on('keyup', function () {
     echoInput($(this).val(), 'greetPlayer')
 
   });
 
+
+
+  // todo: move this into a drop.js script
   $(function () {
     $("#gamePiece").draggable();
     $("#winGame").droppable({
@@ -27,6 +65,16 @@ $(() => { //super shorthand
   });
 });
 
+
+//check selected answers
+function checkAnswers(){
+  // if choice is selected, and answer, return WIN 
+  if(choice == answer){
+    score += 1;
+    $('#gameScore').text(`Score: ${score}`);
+    // return 1; //if true, win with score +1  
+  }
+}
 
 // document.addEventListener('DOMContentLoaded', function() {
 //     console.log('dom content is loaded');
